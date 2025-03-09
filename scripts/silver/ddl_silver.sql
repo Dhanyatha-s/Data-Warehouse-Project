941acebc5319555eadab1eaecb6cb7f3e75e198e
Dhanyatha S <95542660+Dhanyatha-s@users.noreply.github.com>
2025-03-09 17:04:14 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -20,21 +20,20 @@ CREATE TABLE silver.crm_cust_info (
 );
 GO
 
-IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE silver.crm_prd_info;
+IF OBJECT_ID('silver.crm_prd_info' , 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info
 GO
-
-CREATE TABLE silver.crm_prd_info (
-    prd_id          INT,
-    cat_id          NVARCHAR(50),
-    prd_key         NVARCHAR(50),
-    prd_nm          NVARCHAR(50),
-    prd_cost        INT,
-    prd_line        NVARCHAR(50),
-    prd_start_dt    DATE,
-    prd_end_dt      DATE,
-    dwh_create_date DATETIME2 DEFAULT GETDATE()
-);
+create table silver.crm_prd_info(
+prd_id INT,
+cat_id NVARCHAR(50),
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATE,
+prd_end_dt DATE,
+dwh_create_date    DATETIME2 DEFAULT GETDATE()
+)
 GO
 
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
